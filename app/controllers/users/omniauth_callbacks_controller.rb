d368e7ac682d4048c9844dc95b78b0f2f07a06f5
zainrauf <zain.rauf@tiksom.com>
2018-03-29 14:35:24 +0500

    authenicate off in onmni controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -22,7 +22,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     puts @user
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user#, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"]
